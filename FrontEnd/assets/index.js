33fd7526f99b3b144f130c406277b67900e4e2b8
Olivier <baumert.olivier@gmail.com>
2024-11-25 16:09:54 +0100

    Ajout images & text

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,33 +1,26 @@
-// const reponse = await fetch('http://localhost:5678/api/works');
-// const works = await reponse.json();
-// console.log(works);
+const reponse = await fetch('http://localhost:5678/api/works');
+const works = await reponse.json();
+console.log(works);
 
-// /* works.forEach((work) => {
-//     createHTML();
-// }); */
+for (let i = 0; i < works.length; i++) {
+createHTML(works[i]);
+}
 
-// for (let i = 0; i < works.length; i++) {
-//     /*  console.log(works[i].title); */
-//     createHTML(works[i]);
-// }
+function createHTML(work) {
+   console.log(work);
+  let figure = document.createElement('figure');
+  let figureImg = document.createElement('img');
+   figureImg.src = work.imageUrl;
+  figureImg.alt = work.category.name;
 
-// function createHTML(work) {
-//     console.log(work.title);
-//     let figure = document.createElement('figure');
-//     let figureImg = document.createElement('img');
-//     figureImg.src = 'assets/images/abajour-tahina.png';
-//     figureImg.alt = 'Abajour Tahina';
+ let figureFigcaption = document.createElement('figcaption');
+  figureFigcaption.innerText = work.title;
 
-//     let figureFigcaption = document.createElement('figcaption');
-//     figureFigcaption.innerText = work.title;
 
-//     figure.appendChild(figureImg);
-//     figure.appendChild(figureFigcaption);
 
-//     let gallery = document.querySelector('.gallery');
-//     gallery.appendChild(figure);
-// }
+ figure.appendChild(figureImg);
+  figure.appendChild(figureFigcaption);
 
-const response = await fetch("http://localhost:5678/api/works") 
-const works = await response.json()
-console.log(works)
+ let gallery = document.querySelector('.gallery');
+   gallery.appendChild(figure);
+}
