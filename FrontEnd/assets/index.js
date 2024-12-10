a0ed5f13a96dbbcc2d7c71b9e518e97472652934
Olivier <baumert.olivier@gmail.com>
2024-12-10 15:55:08 +0100

    Ajout boutons dynamiques

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,5 +1,5 @@
-const reponse = await fetch('http://localhost:5678/api/works');
-const works = await reponse.json();
+const reponseWorks = await fetch('http://localhost:5678/api/works');
+const works = await reponseWorks.json();
 console.log(works);
 
 for (let i = 0; i < works.length; i++) {
@@ -7,20 +7,41 @@ createHTML(works[i]);
 }
 
 function createHTML(work) {
-   console.log(work);
-  let figure = document.createElement('figure');
-  let figureImg = document.createElement('img');
-   figureImg.src = work.imageUrl;
-  figureImg.alt = work.category.name;
+console.log(work);
+let figure = document.createElement('figure');
+let figureImg = document.createElement('img');
+figureImg.src = work.imageUrl;
+figureImg.alt = work.category.name;
 
- let figureFigcaption = document.createElement('figcaption');
-  figureFigcaption.innerText = work.title;
+let figureFigcaption = document.createElement('figcaption');
+figureFigcaption.innerText = work.title;
 
+figure.appendChild(figureImg);
+figure.appendChild(figureFigcaption);
 
+let gallery = document.querySelector('.gallery');
+gallery.appendChild(figure);
+}
 
- figure.appendChild(figureImg);
-  figure.appendChild(figureFigcaption);
+const buttonTous = document.createElement("button");
+const containerBoutons = document.querySelector(".boutons");
+buttonTous.innerText = "Tous";
+containerBoutons.appendChild(buttonTous);
 
- let gallery = document.querySelector('.gallery');
-   gallery.appendChild(figure);
+buttonTous.addEventListener("click", () => {
+    console.log("Tous");
+  });
+
+const reponseCategories = await fetch('http://localhost:5678/api/categories');
+const categories = await reponseCategories.json();
+console.log(categories[0].name);
+
+for (let i = 0; i < categories.length; i++) {
+    let boutons = document.createElement("button")
+    boutons.innerText = categories[i].name;
+    const containerBoutons = document.querySelector('.boutons');
+    containerBoutons.appendChild(boutons);
+    boutons.addEventListener("click", () => {
+    console.log(categories[i].name);
+    });    
 }
